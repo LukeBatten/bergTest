e70a510e7f8261a655d09c37e5f35a00c96645b4
Luke Batten <luke.batten.15@ucl.ac.uk>
2017-04-11 14:10:13 +0100

    adding csv reader

---
+++ He2/plotIndivHelium_C.d
@@ -0,0 +1,122 @@
+
+# DO NOT DELETE
+
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/AnitaConventions.h
+./plotIndivHelium_C.so: /usr/include/root/Rtypes.h
+./plotIndivHelium_C.so: /usr/include/root/RConfig.h
+./plotIndivHelium_C.so: /usr/include/root/RVersion.h
+./plotIndivHelium_C.so: /usr/include/root/DllImport.h
+./plotIndivHelium_C.so: /usr/include/root/Rtypeinfo.h
+./plotIndivHelium_C.so: /usr/include/root/snprintf.h
+./plotIndivHelium_C.so: /usr/include/root/strlcpy.h
+./plotIndivHelium_C.so: /usr/include/root/TGenericClassInfo.h
+./plotIndivHelium_C.so: /usr/include/root/TSchemaHelper.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/AnitaVersion.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/UsefulAnitaEvent.h
+./plotIndivHelium_C.so: /usr/include/root/TObject.h
+./plotIndivHelium_C.so: /usr/include/root/TStorage.h
+./plotIndivHelium_C.so: /usr/include/root/TVersionCheck.h
+./plotIndivHelium_C.so: /usr/include/root/Riosfwd.h
+./plotIndivHelium_C.so: /usr/include/root/TBuffer.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/RawAnitaEvent.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/simpleStructs.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/oldStructs.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/AnitaGeomTool.h
+./plotIndivHelium_C.so: /usr/include/root/TMath.h
+./plotIndivHelium_C.so: /usr/include/root/TMathBase.h
+./plotIndivHelium_C.so: /usr/include/root/TError.h
+./plotIndivHelium_C.so: /usr/include/root/TVector3.h
+./plotIndivHelium_C.so: /usr/include/root/TVector2.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrix.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixF.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixT.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixTBase.h
+./plotIndivHelium_C.so: /usr/include/root/TNamed.h
+./plotIndivHelium_C.so: /usr/include/root/TString.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixFBasefwd.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixDBasefwd.h
+./plotIndivHelium_C.so: /usr/include/root/TVectorFfwd.h
+./plotIndivHelium_C.so: /usr/include/root/TVectorDfwd.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixTUtils.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixFfwd.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixFUtils.h
+./plotIndivHelium_C.so: /usr/include/root/TMatrixFUtilsfwd.h
+./plotIndivHelium_C.so: /usr/include/root/TObjArray.h
+./plotIndivHelium_C.so: /usr/include/root/TSeqCollection.h
+./plotIndivHelium_C.so: /usr/include/root/TCollection.h
+./plotIndivHelium_C.so: /usr/include/root/TIterator.h
+./plotIndivHelium_C.so: /usr/include/root/TObjString.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/RawAnitaHeader.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/TimedAnitaHeader.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/PrettyAnitaHk.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/CalibratedHk.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/FFTtools.h
+./plotIndivHelium_C.so: /usr/include/root/TGraph.h
+./plotIndivHelium_C.so: /usr/include/root/TAttLine.h
+./plotIndivHelium_C.so: /usr/include/root/TAttFill.h
+./plotIndivHelium_C.so: /usr/include/root/TAttMarker.h
+./plotIndivHelium_C.so: /usr/include/root/TFitResultPtr.h
+./plotIndivHelium_C.so: /usr/include/root/TROOT.h
+./plotIndivHelium_C.so: /usr/include/root/TDirectory.h
+./plotIndivHelium_C.so: /usr/include/root/TList.h /usr/include/root/TDatime.h
+./plotIndivHelium_C.so: /usr/include/root/TUUID.h /usr/include/root/TSystem.h
+./plotIndivHelium_C.so: /usr/include/root/TInetAddress.h
+./plotIndivHelium_C.so: /usr/include/root/TTimer.h
+./plotIndivHelium_C.so: /usr/include/root/TSysEvtHandler.h
+./plotIndivHelium_C.so: /usr/include/root/TQObject.h
+./plotIndivHelium_C.so: /usr/include/root/TTime.h
+./plotIndivHelium_C.so: /usr/include/root/ThreadLocalStorage.h
+./plotIndivHelium_C.so: /usr/include/root/RConfigure.h
+./plotIndivHelium_C.so: /usr/include/root/Math/Interpolator.h
+./plotIndivHelium_C.so: /usr/include/root/Math/InterpolationTypes.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/FFTWindow.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/FFTWComplex.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/RFSignal.h
+./plotIndivHelium_C.so: /usr/include/root/TLegend.h /usr/include/root/TPave.h
+./plotIndivHelium_C.so: /usr/include/root/TBox.h
+./plotIndivHelium_C.so: /usr/include/root/TAttBBox2D.h
+./plotIndivHelium_C.so: /usr/include/root/GuiTypes.h
+./plotIndivHelium_C.so: /usr/include/root/TPoint.h
+./plotIndivHelium_C.so: /usr/include/root/TAttText.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/TurfRate.h
+./plotIndivHelium_C.so: /home/batten/ANITA_util/include/Adu5Pat.h
+./plotIndivHelium_C.so: /usr/include/root/TFile.h
+./plotIndivHelium_C.so: /usr/include/root/TDirectoryFile.h
+./plotIndivHelium_C.so: /usr/include/root/TMap.h
+./plotIndivHelium_C.so: /usr/include/root/THashTable.h
+./plotIndivHelium_C.so: /usr/include/root/TUrl.h /usr/include/root/TTree.h
+./plotIndivHelium_C.so: /usr/include/root/TBranch.h
+./plotIndivHelium_C.so: /usr/include/root/TDataType.h
+./plotIndivHelium_C.so: /usr/include/root/TDictionary.h
+./plotIndivHelium_C.so: /usr/include/root/Property.h
+./plotIndivHelium_C.so: /usr/include/root/ESTLType.h
+./plotIndivHelium_C.so: /usr/include/root/TArrayD.h
+./plotIndivHelium_C.so: /usr/include/root/TArray.h
+./plotIndivHelium_C.so: /usr/include/root/TArrayI.h
+./plotIndivHelium_C.so: /usr/include/root/TClass.h
+./plotIndivHelium_C.so: /usr/include/root/TVirtualTreePlayer.h
+./plotIndivHelium_C.so: /usr/include/root/TChain.h
+./plotIndivHelium_C.so: /usr/include/root/TCanvas.h /usr/include/root/TPad.h
+./plotIndivHelium_C.so: /usr/include/root/TVirtualPad.h
+./plotIndivHelium_C.so: /usr/include/root/TAttPad.h
+./plotIndivHelium_C.so: /usr/include/root/TVirtualX.h
+./plotIndivHelium_C.so: /usr/include/root/Buttons.h
+./plotIndivHelium_C.so: /usr/include/root/TAttCanvas.h
+./plotIndivHelium_C.so: /usr/include/root/TCanvasImp.h
+./plotIndivHelium_C.so: /usr/include/root/TH2.h /usr/include/root/TH1.h
+./plotIndivHelium_C.so: /usr/include/root/TAxis.h
+./plotIndivHelium_C.so: /usr/include/root/TAttAxis.h
+./plotIndivHelium_C.so: /usr/include/root/TArrayC.h
+./plotIndivHelium_C.so: /usr/include/root/TArrayS.h
+./plotIndivHelium_C.so: /usr/include/root/TArrayF.h
+./plotIndivHelium_C.so: /usr/include/root/Foption.h
+./plotIndivHelium_C.so: /usr/include/root/TColor.h /usr/include/root/TImage.h
+./plotIndivHelium_C.so: /usr/include/root/TAttImage.h
+./plotIndivHelium_C.so: /usr/include/root/TStyle.h
+./plotIndivHelium_C.so: /usr/include/root/TMarker.h
+./plotIndivHelium_C.so: /usr/include/root/TLatex.h /usr/include/root/TText.h
+./plotIndivHelium_C.so: /usr/include/root/TEllipse.h
+./plotIndivHelium_C.so: /usr/include/root/TLine.h
+./plotIndivHelium_C.so: /usr/include/root/TPaveText.h
+./plotIndivHelium_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+plotIndivHelium_C__ROOTBUILDVERSION= 5.34/36
---
+++ testMix_cxx.d
@@ -0,0 +1,4 @@
+
+# DO NOT DELETE
+./testMix_cxx.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+testMix_cxx__ROOTBUILDVERSION= 5.34/36
