547e80ed62e46bfe6e079e8f3e214f71d7b1f6a3
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-04-11 15:59:29 +0100

    rootified base data

---
+++ testMix2.cxx
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+void testMix2()
+{
+
+  TFile f("tree1.root","recreate");
+  TTree t1("t1","a simple Tree with simple variables");
+
+
+  // BASE LIST VARS:
+  std::string name;
+  int latDeg;
+  double latMin;
+  std::string latCar;
+  int longDeg;
+  double longMin;
+  std::string longCar;
+  //double alt = std::stoi(7]); // will crash, as altitude isn't filled for all fields
+  std::string primaryOperator;
+  //int yearEst = std::stoi(9]);
+  std::string facType;
+  std::string seasonality;
+  std::string currentStatus;
+
+  t1.Branch("name",&name);
+  
+  t1.Branch("latDeg",&latDeg,"latDeg/I");
+  t1.Branch("latMin",&latMin,"latMin/D");
+  t1.Branch("latCar",&latCar);
+  
+  t1.Branch("longDeg",&longDeg,"longDeg/I");
+  t1.Branch("longMin",&longMin,"longMin/D");
+  t1.Branch("longCar",&longCar);
+
+  t1.Branch("primaryOperator",&primaryOperator);
+  t1.Branch("facType",&facType);
+  t1.Branch("seasonality",&seasonality);
+  t1.Branch("currentStatus",&currentStatus);
+
+  // CUSTOM VARS (calculated from the base list)
+  
+  // Load base list
+  std::ifstream ifile("base_list-A4-unrestricted.csv");
+
+  std::string line; 
+  while (std::getline(ifile, line))
+    {
+      
+      std::istringstream iss(line); // construct a string stream from line
+
+      // read the tokens from current line separated by comma
+      std::vector<std::string> tokens; // here we store the tokens
+      std::string token; // current token
+      while (std::getline(iss, token, ','))
+        {
+	  tokens.push_back(token); // add the token to the vector
+        }
+
+      // map the tokens into our variables1
+      name = tokens[0];
+      latDeg = std::stoi(tokens[1]);
+      latMin = std::stof(tokens[2]);
+      latCar = tokens[3];
+      longDeg = std::stoi(tokens[4]);
+      longMin = std::stof(tokens[5]);
+      longCar = tokens[6];
+      //double alt = std::stof(tokens[7]); // will crash, as altitude isn't filled for all fields
+      primaryOperator = tokens[8];
+      //int yearEst = std::stoi(tokens[9]);
+      facType = tokens[10];
+      seasonality = tokens[11];
+      currentStatus = tokens[12];
+
+      t1.Fill();
+
+      /*
+      std::cout << " " << std::endl;
+      std::cout << "\t Name: " << name << std::endl;
+      std::cout << "\t Lat (deg): " << latDeg << std::endl;
+      std::cout << "\t Lat (mins): " << latMin << std::endl;
+      std::cout << "\t Lat cardinality: " << latCar << std::endl;
+      std::cout << "\t Long (deg): " << longDeg << std::endl;
+      std::cout << "\t Long (mins): " << longMin << std::endl;
+      std::cout << "\t Long cardinality: " << longCar << std::endl;
+      //std::cout << "\t Altitude: " << alt << std::endl;
+      std::cout << "\t Primary operator: " << primaryOperator << std::endl;
+      //std::cout << "\t Year established: " << yearEst << std::endl;
+      std::cout << "\t Facility type: " << facType << std::endl;
+      std::cout << "\t Seasonality: " << seasonality << std::endl;
+      std::cout << "\t Current status: " << currentStatus << std::endl;
+*/
+      
+    }
+
+  t1.Write();
+  std::cout << "Tree written" << std::endl;
+    
+
+  return;
+}
