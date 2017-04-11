a51ac0f010d0e9186f809fd07281a3e85c6db8cd
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-04-11 21:15:04 +0100

    Add support for spreadsheets with multiple sheets

--- makeANITA4baseCSVs.sh
+++ makeANITA4baseCSVs.sh
@@ -6,20 +6,24 @@ rm -rf base_list-A4-unrestricted.*
 
 # Just do for ANITA-4 for now
 wget -N "https://www.phys.hawaii.edu/elog/anita_notes/170409_223955/base_list-A4-unrestricted.ods" # overwrite!
+
 echo "Converting the spreadsheet to csv"
 
-soffice --headless --convert-to csv base_list-A4-unrestricted.ods
-unoconv -f ods -i FilterOptions="9,34,UNICODE,1" base_list-A4-unrestricted.ods # convert to csv
+# converts spreadsheets into individual sheets
+ssconvert -S base_list-A4-unrestricted.ods base_list-A4-unrestricted.csv
 
 # get rid of first 2 headers
-sed -i '1,2 d' base_list-A4-unrestricted.csv
+sed -i '1,2 d' base_list-A4-unrestricted.csv.0
 
 # in case there are any empty fields, replace them with -999
-sed -i 's/,\{2,\}/,-999,/g' base_list-A4-unrestricted.csv
+sed -i 's/,\{2,\}/,-999,/g' base_list-A4-unrestricted.csv.0
+
+# replaced exported quotes with nothing
+sed -i 's/"//g' base_list-A4-unrestricted.csv.0
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD
 root -b -l <<EOF
-   .X testMix2.cxx
+   .X basesCampsRootifier.cxx
 EOF
 
 echo "Done!"
