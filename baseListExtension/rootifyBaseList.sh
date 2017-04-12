730d0e41230a33894724ccfc62b88c75838022a4
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-04-12 20:30:56 +0100

    up-to-date comment

--- baseListExtension/rootifyBaseList.sh
+++ baseListExtension/rootifyBaseList.sh
@@ -9,7 +9,7 @@ rm -rf base_list-A3-unrestricted.*
 rm -f baseList.root
 rm -f baseListA3.root
 
-# Just do for ANITA-4 for now
+# Get most recent from the elog. CHECK the string after anita_notes
 echo "Pulling the newest spreadsheets from the elog"
 wget -N "https://www.phys.hawaii.edu/elog/anita_notes/170409_223955/base_list-A4-unrestricted.ods" # overwrite!
 wget -N "https://www.phys.hawaii.edu/elog/anita_notes/170410_214006/base_list-A3-unrestricted.ods" # overwrite!
@@ -44,9 +44,17 @@ sed -i 's/"//g' base_list-A3-unrestricted.csv.0
 sed -i 's/"//g' base_list-A3-unrestricted.csv.1
 sed -i 's/"//g' base_list-A3-unrestricted.csv.2
 
+# replace approximations (~) with nothing
+sed -i 's/~//g' base_list-A4-unrestricted.csv.0
+sed -i 's/~//g' base_list-A4-unrestricted.csv.1
+sed -i 's/~//g' base_list-A4-unrestricted.csv.2
+sed -i 's/~//g' base_list-A3-unrestricted.csv.0
+sed -i 's/~//g' base_list-A3-unrestricted.csv.1
+sed -i 's/~//g' base_list-A3-unrestricted.csv.2
+
 # Move csvs to a folder and tar 'em up
 mkdir baseListCSVs
-mv base_list-A3-unrestricted.csv.0 base_list-A3-unrestricted.csv.1 base_list-A3-unrestricted.csv.2 base_list-A4-unrestricted.csv.0 base_list-A4-unrestricted.csv.1 base_list-A4-unrestricted.csv.2 baseListCSVs
+cp base_list-A3-unrestricted.csv.0 base_list-A3-unrestricted.csv.1 base_list-A3-unrestricted.csv.2 base_list-A4-unrestricted.csv.0 base_list-A4-unrestricted.csv.1 base_list-A4-unrestricted.csv.2 baseListCSVs
 tar -zcvf baseListCSVs.tar.gz baseListCSVs
 
 echo "Running rootfication scripts"
@@ -55,7 +63,7 @@ root -b -l <<EOF
    .X basesCampsRootifier.cxx
    .X fixedWingRootifier.cxx	
    .X awsRootifier.cxx
-   #.X basesCampsRootifierA3.cxx
+   .X basesCampsRootifierA3.cxx
    .X fixedWingRootifierA3.cxx	
    .X awsRootifierA3.cxx
 EOF
